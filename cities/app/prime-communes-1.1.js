e023b52c15020baef1bd8dc41f0ef483806d0549
AxelK <obersax@gmail.com>
2026-09-14 20:03:04 +0200

    Refresh NEWS handoff script

--- cities/app/prime-communes-1.1.js
+++ cities/app/prime-communes-1.1.js
@@ -32,7 +32,7 @@
       loadScript('app/prime-communes-1.1-base.js?v=2', () => {
         loadScript('app/prime-communes-stats-1.5.js?v=4', () => {
           loadScript('app/prime-communes-communes-1.2.js?v=4', () => {
-            loadScript('app/prime-communes-news-2.0.js?v=4', () => {
+            loadScript('app/prime-communes-news-2.0.js?v=5', () => {
               loadScript('app/prime-communes-roadmap-1.2.js?v=2');
             });
           });
