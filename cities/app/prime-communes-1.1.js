8e632347f04e6d6b9a6dc73eb8b9a995dd87654c
AxelK <obersax@gmail.com>
2026-09-14 20:01:35 +0200

    Refresh responsive commune header assets

--- cities/app/prime-communes-1.1.js
+++ cities/app/prime-communes-1.1.js
@@ -19,7 +19,7 @@
     document.body.append(script);
   };
 
-  loadStyle('app/prime-communes-1.1.5.css?v=8');
+  loadStyle('app/prime-communes-1.1.5.css?v=9');
   loadStyle('app/prime-communes-stats-1.2.css?v=11');
   loadStyle('app/prime-communes-maplibre-1.2.css?v=10');
   loadStyle('app/prime-communes-news-2.0.css?v=3');
