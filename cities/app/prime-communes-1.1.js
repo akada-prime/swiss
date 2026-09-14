20b934592791bdf483c5ab3d395868ef5bf885b4
AxelK <obersax@gmail.com>
2026-09-14 20:31:08 +0200

    NEWS!: diffuser la règle Client Prime corrigée

--- cities/app/prime-communes-1.1.js
+++ cities/app/prime-communes-1.1.js
@@ -32,7 +32,7 @@
       loadScript('app/prime-communes-1.1-base.js?v=4', () => {
         loadScript('app/prime-communes-stats-1.5.js?v=4', () => {
           loadScript('app/prime-communes-communes-1.2.js?v=4', () => {
-            loadScript('app/prime-communes-news-2.0.js?v=6', () => {
+            loadScript('app/prime-communes-news-2.0.js?v=7', () => {
               loadScript('app/prime-communes-roadmap-1.2.js?v=2');
             });
           });
