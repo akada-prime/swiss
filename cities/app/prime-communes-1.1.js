ba82a0560211a437035cfed1901a679bd96b0c46
AxelK <obersax@gmail.com>
2026-09-20 10:23:36 +0200

    Add universal mobile commune search (2/7)

--- cities/app/prime-communes-1.1.js
+++ cities/app/prime-communes-1.1.js
@@ -19,7 +19,7 @@
     document.body.append(script);
   };
 
-  loadStyle('app/prime-communes-1.1.5.css?v=13');
+  loadStyle('app/prime-communes-1.1.5.css?v=14');
   loadStyle('app/prime-communes-stats-1.2.css?v=11');
   loadStyle('app/prime-communes-maplibre-1.2.css?v=11');
   loadStyle('app/prime-communes-news-2.0.css?v=7');
@@ -32,7 +32,7 @@
     loadScript('app/prime-communes-maplibre-1.2.js?v=11', () => {
       loadScript('app/prime-communes-1.1-base.js?v=8', () => {
         loadScript('app/prime-communes-stats-1.5.js?v=4', () => {
-          loadScript('app/prime-communes-communes-1.2.js?v=5', () => {
+          loadScript('app/prime-communes-communes-1.2.js?v=6', () => {
             loadScript('app/prime-communes-news-2.0.js?v=10', () => {
               loadScript('app/prime-communes-roadmap-1.2.js?v=2');
             });
